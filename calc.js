caa8083304044c615c8931e654e250d2e18a255c
saicharan1100 <saic8688@gmail.com>
2025-03-05 21:14:21 +0530

    omg

--- calc.js
+++ calc.js
@@ -1,9 +1,15 @@
 var str="";
 
 function display(a){
-   if(a != '='){
+   if((a==str.charAt(str.length-1)&&(a=='+'||a=='-'||a=='*'||a=='/'||a=='('||a==')'||a=='.'))){
+      document.getElementById('floatingTextarea').innerHTML=str;
+   }
+  else if( (( str.charAt(str.length-1) =='-')|| ( str.charAt(str.length-1) =='+')|| ( str.charAt(str.length-1) =='/')|| ( str.charAt(str.length-1) =='*')||( str.charAt(str.length-1) =='(')||( str.charAt(str.length-1) ==')')||( str.charAt(str.length-1) =='.')) &&(a=='+'||a=='-'||a=='/'||a=='*'||a=='.'||a=='('||a==')')){
+   document.getElementById('floatingTextarea').innerHTML=str;
+  }
+  else if(a != '='){
       str+=a;
-      document.getElementById('exampleFormControlTextarea1').innerHTML=str;
+      document.getElementById('floatingTextarea').innerHTML=str;
    }
    else{
      
@@ -12,4 +18,16 @@ function display(a){
    }
 
 }
+function alclr(){
+    str=""
+    document.getElementById('floatingTextarea').innerHTML=str;
+    document.getElementById('opt').innerHTML= str;
 
+
+}
+
+function del(){
+   str=str.substring(0,str.length-1);
+   document.getElementById('floatingTextarea').innerHTML=str;
+
+}
