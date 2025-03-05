71643ace4a3a0ac3aee58709c6ba5048581083f7
saicharan1100 <saic8688@gmail.com>
2025-03-05 23:50:59 +0530

    push

--- calc.js
+++ calc.js
@@ -1,25 +1,56 @@
 var str="";
-
+var result=0;
 function display(a){
-   if((a==str.charAt(str.length-1)&&(a=='+'||a=='-'||a=='*'||a=='/'||a=='('||a==')'||a=='.'))){
+   if((a==str.charAt(str.length-1)&&(a=='+'||a=='-'||a=='*'||a=='/'||a=='.'))){
       document.getElementById('floatingTextarea').innerHTML=str;
+      alert("Enter Number");
+ 
    }
-  else if( (( str.charAt(str.length-1) =='-')|| ( str.charAt(str.length-1) =='+')|| ( str.charAt(str.length-1) =='/')|| ( str.charAt(str.length-1) =='*')||( str.charAt(str.length-1) =='(')||( str.charAt(str.length-1) ==')')||( str.charAt(str.length-1) =='.')) &&(a=='+'||a=='-'||a=='/'||a=='*'||a=='.'||a=='('||a==')')){
+   
+else if( ( (str.charAt(str.length-1)!=('+')&&str.charAt(str.length-1)!=('-')&&str.charAt(str.length-1)!=('*')&&str.charAt(str.length-1)!=('/')&&str.charAt(str.length-1)!=('.'))&&(str!="")  )&&(a=='(')             ){
+str+='*';
+str+='(';
+document.getElementById('floatingTextarea').innerHTML=str;
+
+}
+   else if(str==""&&(a=='+'||a=='-'||a=='*'||a=='/')){
+str+="0"+a;
+document.getElementById('floatingTextarea').innerHTML=str;
+
+   }
+  else if( (( str.charAt(str.length-1) =='-')|| ( str.charAt(str.length-1) =='+')|| ( str.charAt(str.length-1) =='/')|| ( str.charAt(str.length-1) =='*')||( str.charAt(str.length-1) =='.')) &&(a=='+'||a=='-'||a=='/'||a=='*')){
    document.getElementById('floatingTextarea').innerHTML=str;
+   alert("Enter Number");
+  }
+  else if(( str.charAt(str.length-1) =='.')&&(a=='('||a==')')){
+   document.getElementById('floatingTextarea').innerHTML=str;
+   alert("Enter Number");
+
   }
   else if(a != '='){
       str+=a;
       document.getElementById('floatingTextarea').innerHTML=str;
    }
    else{
-     
-      document.getElementById('opt').innerHTML= a+eval(str);
+      if(str==""){
+         document.getElementById('opt').innerHTML=str;
+      }
+      else{
+         try{
+     result=eval(str);
+      document.getElementById('opt').innerHTML= a+result;
+      }
+      catch(error){
+         document.getElementById('opt').innerHTML= "Invalid input";
 
+      }
+   }
    }
 
 }
 function alclr(){
-    str=""
+    str="";
+    result=0;
     document.getElementById('floatingTextarea').innerHTML=str;
     document.getElementById('opt').innerHTML= str;
 
@@ -29,5 +60,6 @@ function alclr(){
 function del(){
    str=str.substring(0,str.length-1);
    document.getElementById('floatingTextarea').innerHTML=str;
+   document.getElementById('opt').innerHTML= "";
 
 }
